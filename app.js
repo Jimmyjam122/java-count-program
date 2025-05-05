3d2104709fb42ac6c4b824bbb8a36068913ef257
Jimmyjam122 <jimmytshimi@gmail.com>
2025-05-05 00:51:49 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const decrease = document.getElementById("decrease")
+const increase = document.getElementById("increase")
+const reset = document.getElementById("reset")
+const countlabel = document.getElementById("countlabel")
+
+let count = 0;
+
+increase.onclick =function () {
+    count ++;
+    countlabel.textContent = count;
+}
+
+decrease.onclick = function(){
+    count--;
+    countlabel.textContent = count;
+}
+
+reset.onclick = function() {
+    count = 0;
+    countlabel.textContent = count;
+}
